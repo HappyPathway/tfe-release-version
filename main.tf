075b5855c211226fbfc11726b6aec94a838a4a2c
David Arnold <dja+djaboxx@sonic.net>
2018-09-22 16:26:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ variable "service_version" {
     default = "1.0.0"
 }
 
+variable "set_version" {
+    default = true
+}
+
 variable "consul_cluster" {}
 
 variable "consul_dc" {}
@@ -16,6 +20,7 @@ provider "consul" {
 }
 
 data "consul_key_prefix" "app" {
+  count = "${var.set_version ? 0 : 1 }"
   datacenter = "${var.consul_dc}"
   # token      = "abcd"
 
@@ -30,7 +35,38 @@ data "consul_key_prefix" "app" {
   }
 }
 
+resource "consul_keys" "app" {
+  count = "${var.set_version ? 1 : 0 }"
+  datacenter = "${var.datacenter}"
+  # token      = "abcd"
+
+  # Set the CNAME of our load balancer as a key
+  key {
+    path    = "${var.service_name}/release_version"
+    value = "${var.service_version}"
+  }
+}
+
+data "consul_key_prefix" "app_set" {
+  count = "${var.set_version ? 1 : 0 }"
+  depends_on = [
+      "consul_keys.app"
+  ]
+  datacenter = "${var.consul_dc}"
+  # token      = "abcd"
+
+  # Prefix to add to prepend to all of the subkey names below.
+  path_prefix = "app/deployments"
+
+  # Read the release version
+  subkey {
+    name    = "release_version"
+    path    = "${var.service_name}/release_version"
+    default = "${var.service_version}"
+  }
+}
+    
 output "release_version" {
-    value = "${data.consul_key_prefix.app.var.release_version}"
+    value = "${var.set_version ? data.consul_key_prefix.app_set.var.release_version : data.consul_key_prefix.app.var.release_version}"
 }
 
